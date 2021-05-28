5d0506bfe1a4100530bb4a0a7e8a43e1aca33589
Dvaid Adeleye <drizzzydave@gmail.com>
2021-05-28 12:49:09 +0100

    updates to the nav bar, added menu on small screens

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -4,13 +4,14 @@
             <p class="pl-4">Pexels</p>
         </div>
         <div class="flex "> 
-            <ul class="flex justify-center w-full items-center"> 
+            <ul class=" hidden md:flex justify-center w-full items-center"> 
                 <li class="pr-6"> Explore </li>
                   <li class="pr-6"> License</li>
-                    <li class="pr-6"> HEl</li>
-                      <li class="pr-6">Ther </li>
+                    <li class="pr-6"> <span class="icon is-left "><font-awesome-icon class="" :icon="['fa', 'bell']"> </font-awesome-icon></span></li>
+                      <li class="pr-6"><span class="icon is-left "><font-awesome-icon class="" :icon="['fa', 'user-circle']"> </font-awesome-icon></span></li>
                       <li><button class="bg-green-600 px-4 h-10  mr-6 rounded font-bold"> Upload </button></li>
             </ul>
+            <span class="icon is-left inline md:hidden pr-3"><font-awesome-icon class="" :icon="['fa', 'bars']"> </font-awesome-icon></span>
         </div>
     </div>
 </template>
